b29fce20b3c733a0711bb16ec078c5bd5bc3d418
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2023-09-02 02:28:28 +0530

    added readme file

---
+++ README.md
@@ -0,0 +1,52 @@
+# Heart Disease Prediction App
+![Project Screenshot](./heartApp.png)
+
+This project is an application for predicting heart disease using Artificial Neural Networks (ANN). It is built with Streamlit for the user interface and hosted on the Streamlit platform.
+
+## Project Overview
+
+- Predicted heart disease using various machine learning algorithms:
+  - Logistic Regression (LR)
+  - Naive Bayes (NB)
+  - Support Vector Machine (SVM)
+  - k-Nearest Neighbors (KNN)
+  - Artificial Neural Network (ANN)
+  - Convolutional Neural Network (CNN)
+
+- Achieved impressive testing accuracy:
+  - ANN-based model: Approximately 98.54%
+  - Second-best model: CNN with an accuracy of 96.58%
+
+- Utilized Boruta feature selection technique:
+  - Reduced features from 13 to 10, a 15.38% reduction.
+  - Maintained ANN model accuracy at 98.54%.
+
+- Notable result:
+  - Without feature selection, using the entire dataset, the ANN model achieved a similar test accuracy of around 98.56%.
+
+
+## Dataset
+
+The dataset used for this project can be found on Kaggle at the following link:
+[Heart Disease Dataset](https://www.kaggle.com/datasets/johnsmith88/heart-disease-dataset)
+
+
+## Jupyter Notebook
+
+- For a detailed analysis of the dataset and code used in this project, refer to the [Jupyter Notebook](./main_final(with_all_metrics)_real.ipynb).
+
+
+[Link to Jupyter Notebook](jupyter_notebook.md)
+
+## Live Project
+
+You can access the live project by following this link:
+[Heart Disease Prediction App](https://naveenkumardongre-heart-disease-prediction-app-c1px5m.streamlit.app/)
+
+## Instructions for Running the App Locally
+
+1. Clone this repository to your local machine.
+2. Install the required libraries using `pip install -r requirements.txt`.
+3. Run the Streamlit app using the command `streamlit run app.py`.
+
+Enjoy predicting heart disease with our app!
--- app.py
+++ app.py
@@ -5,70 +5,22 @@ import numpy as np
 import pickle as pkl
 from sklearn.preprocessing import StandardScaler
 scal=StandardScaler()
+
 #Load the saved model
-model=pkl.load(open("final_model.p","rb"))
-
-
-
-
+# model=pkl.load(open("final_model.p","rb"))
+model=pkl.load(open("model_wcc.pkl","rb"))
 
 st.set_page_config(page_title="Healthy Heart App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
 
-# class predicting method
 
-def predict_disease(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal): 
-    col_names = np.array(['age', 'sex', 'trestbps', 'chol', 'thalach', 'oldpeak', 'cp_1', 'cp_2','cp_3', 'fbs_1', 'restecg_1', 'restecg_2', 'exang_1', 'slope_1','slope_2', 'ca_1', 'ca_2', 'ca_3', 'ca_4', 'thal_1', 'thal_2','thal_3'])
-    cp = "cp_"+str(cp)
-    fbs = "fbs_"+str(fbs)
-    exang = "exang_"+str(exang)
-    slope = "slope_"+str(slope)
-    ca = "ca_"+str(ca)
-    thal = "thal_"+str(thal)
-    restecg = "restecg_"+str(restecg)
-    
-    
-    cp_index = -1 if np.where(col_names==cp)[0].size==0 else np.where(col_names==cp)[0][0]
-    fbs_index = -1 if np.where(col_names==fbs)[0].size==0 else np.where(col_names==fbs)[0][0]
-    exang_index = -1 if np.where(col_names== exang)[0].size == 0 else np.where(col_names==exang)[0][0]
-    slope_index = -1 if np.where(col_names==slope)[0].size == 0 else np.where(col_names==slope)[0][0]
-    ca_index = -1 if np.where(col_names==ca)[0].size == 0 else np.where(col_names==ca)[0][0]
-    thal_index = -1 if np.where(col_names==thal)[0].size == 0 else np.where(col_names==thal)[0][0]
-    restecg_index = -1 if np.where(col_names == restecg)[0].size == 0 else np.where(col_names==restecg)[0][0]
-#     print(cp_index, fbs_index)
-    x = np.zeros(len(col_names))
-    if cp_index >= 0:
-        x[cp_index] = 1
-    if fbs_index >= 0:
-        x[fbs_index] = 1
-    if exang_index >=0:
-        x[exang_index] = 1
-    if slope_index >= 0:
-        x[slope_index] = 1
-    if ca_index >= 0:
-        x[ca_index] = 1
-    if thal_index >= 0:
-        x[thal_index] = 1
-    if restecg_index >= 0:
-        x[restecg_index] = 1
-
-    
-    x[0] = age
-    x[1] = sex
-    x[2] = trestbps
-    x[3] = chol
-    x[4] = thalach
-    x[5] = oldpeak
-
-    
-    # Feature scaling kar raha hu abhi
-    from sklearn.preprocessing import StandardScaler
-    scalar= StandardScaler()
-
-    x[0:5] = scalar.fit_transform(x[0:5].reshape(1,-1))
-
+# Predicting the class
+def predict_disease(x): 
     return model.predict([x])
 
-def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal ):   
+
+#Preprocessing user Input
+# def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal ):   
+def preprocess(age,sex,cp,trestbps,chol,thalach,exang,oldpeak,slope,ca,thal ):   
  
     
     # Pre-processing user input   
@@ -91,10 +43,10 @@ def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,
     elif exang=="No":
         exang=0
  
-    if fbs=="Yes":
-        fbs=1
-    elif fbs=="No":
-        fbs=0
+    # if fbs=="Yes":
+    #     fbs=1
+    # elif fbs=="No":
+    #     fbs=0
  
     if slope=="Upsloping: better heart rate with excercise(uncommon)":
         slope=0
@@ -110,21 +62,67 @@ def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,
     elif thal=="normal":
         thal=2.31
 
-    if restecg=="Nothing to note":
-        restecg=0
-    elif restecg=="ST-T Wave abnormality":
-        restecg=1
-    elif restecg=="Possible or definite left ventricular hypertrophy":
-        restecg=2
+    # if restecg=="Nothing to note":
+    #     restecg=0
+    # elif restecg=="ST-T Wave abnormality":
+    #     restecg=1
+    # elif restecg=="Possible or definite left ventricular hypertrophy":
+    #     restecg=2
+        
+        
+        
+        
+    # col_names = np.array(['age', 'sex', 'trestbps', 'chol', 'thalach', 'oldpeak', 'cp_1', 'cp_2','cp_3', 'fbs_1', 'restecg_1', 'restecg_2', 'exang_1', 'slope_1','slope_2', 'ca_1', 'ca_2', 'ca_3', 'ca_4', 'thal_1', 'thal_2','thal_3'])
+    col_names = np.array(['age', 'sex', 'trestbps', 'chol', 'thalach', 'oldpeak', 'cp_1', 'cp_2','cp_3', 'exang_1', 'slope_1','slope_2', 'ca_1', 'ca_2', 'ca_3', 'ca_4', 'thal_1', 'thal_2','thal_3'])
+    cp = "cp_"+str(cp)
+    # fbs = "fbs_"+str(fbs)
+    exang = "exang_"+str(exang)
+    slope = "slope_"+str(slope)
+    ca = "ca_"+str(ca)
+    thal = "thal_"+str(thal)
+    # restecg = "restecg_"+str(restecg)
+    
+    
+    cp_index = -1 if np.where(col_names==cp)[0].size==0 else np.where(col_names==cp)[0][0]
+    # fbs_index = -1 if np.where(col_names==fbs)[0].size==0 else np.where(col_names==fbs)[0][0]
+    exang_index = -1 if np.where(col_names== exang)[0].size == 0 else np.where(col_names==exang)[0][0]
+    slope_index = -1 if np.where(col_names==slope)[0].size == 0 else np.where(col_names==slope)[0][0]
+    ca_index = -1 if np.where(col_names==ca)[0].size == 0 else np.where(col_names==ca)[0][0]
+    thal_index = -1 if np.where(col_names==thal)[0].size == 0 else np.where(col_names==thal)[0][0]
+    # restecg_index = -1 if np.where(col_names == restecg)[0].size == 0 else np.where(col_names==restecg)[0][0]
+    
+    x = np.zeros(len(col_names))
+    if cp_index >= 0:
+        x[cp_index] = 1
+    # if fbs_index >= 0:
+    #     x[fbs_index] = 1
+    if exang_index >=0:
+        x[exang_index] = 1
+    if slope_index >= 0:
+        x[slope_index] = 1
+    if ca_index >= 0:
+        x[ca_index] = 1
+    if thal_index >= 0:
+        x[thal_index] = 1
+    # if restecg_index >= 0:
+    #     x[restecg_index] = 1
 
+    
+    x[0] = age
+    x[1] = sex
+    x[2] = trestbps
+    x[3] = chol
+    x[4] = thalach
+    x[5] = oldpeak
 
-    # user_input=[age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal]
-    # user_input=np.array(user_input)
-    # user_input=user_input.reshape(1,-1)
-    # user_input=scal.fit_transform(user_input)
-    # prediction = model.predict(user_input)
+    
+    # Feature scaling kar raha hu abhi
+    from sklearn.preprocessing import StandardScaler
+    scalar= StandardScaler()
 
-    return predict_disease(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+    x[0:5] = scalar.fit_transform(x[0:5].reshape(1,-1))
+
+    return x
 
     
 
@@ -138,16 +136,16 @@ html_temp = """
       
 # display the front end aspect
 st.markdown(html_temp, unsafe_allow_html = True) 
-st.subheader('by Naveen Kumar Dongre ')
+st.subheader('by Mescoe Student ')
       
 # following lines create boxes in which user can enter data required to make prediction
 age=st.selectbox ("Age",range(1,121,1))
 sex = st.radio("Select Gender: ", ('male', 'female'))
 cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
 trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
-restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
+# restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
 chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
-fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
+# fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
 thalach=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
 exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
 oldpeak=st.number_input('Oldpeak')
@@ -158,22 +156,21 @@ thal=st.selectbox('Thalium Stress Result',range(1,8,1))
 
 
 #user_input=preprocess(sex,cp,exang, fbs, slope, thal )
-pred=preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+# pred=preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
 
 
-
+# Basically here we are pre-processing the actual user input
+# user_processed_input=preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+user_processed_input=preprocess(age,sex,cp,trestbps,chol,thalach,exang,oldpeak,slope,ca,thal)
+pred = predict_disease(user_processed_input)
 
 if st.button("Predict"):    
   if pred[0] == 0:
-    st.error('Warning! You have high risk of getting a heart attack!')
-    
-  else:
     st.success('You have lower risk of getting a heart disease!')
     
-   
-
-
-
+  else:
+    st.error('Warning! You have high risk of getting a heart attack!')
+    
 st.sidebar.subheader("About App")
 
 st.sidebar.info("This web app is helps you to find out whether you are at a risk of developing a heart disease.")
